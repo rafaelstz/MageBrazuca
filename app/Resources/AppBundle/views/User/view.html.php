6212af69f535ceb800fa8bf8f9c65036c226df41
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2016-01-28 16:00:41 -0200

    Correct Title

--- app/Resources/AppBundle/views/User/view.html.php
+++ app/Resources/AppBundle/views/User/view.html.php
@@ -15,9 +15,9 @@
     <div id="user-info">
         <div id="user-view-image-box">
             <?php if ($user->getPicturePath()) : ?>
-            <img id="user-view-image" src="<?php echo $userHelper->getImageToUserView($user->getPicturePath()); ?>" alt="<?php echo $user->getFullname(); ?>" />
+                <img id="user-view-image" src="<?php echo $userHelper->getImageToUserView($user->getPicturePath()); ?>" alt="<?php echo $user->getFullname(); ?>" />
             <?php else : ?>
-            <img id="user-view-image" src="/images/" alt="<?php echo $user->getFullname(); ?>" />
+                <img id="user-view-image" src="/images/" alt="<?php echo $user->getFullname(); ?>" />
             <?php endif; ?>
         </div>
         <div id="user-view-data-box">
@@ -28,34 +28,34 @@
             <span id="user-view-user-username"><?php echo $user->getUsername(); ?></span>*/ ?>
             <div id="user-view-contacts-box">
                 <?php if ($user->getContactWebsite()) : ?>
-                <a id="user-view-user-website" class="user-view-user-contact" href="<?php echo $user->getContactWebsite(); ?>" target="_blank">
-                    <i class="fa fa-globe tooltip user-view-user-contact-icon" title="Magento Certified Developer"></i>
-                </a>
+                    <a id="user-view-user-website" class="user-view-user-contact" href="<?php echo $user->getContactWebsite(); ?>" target="_blank">
+                        <i class="fa fa-globe tooltip user-view-user-contact-icon" title="Website"></i>
+                    </a>
                 <?php endif; ?>
                 <?php if ($user->getContactTwitter()) : ?>
-                <a id="user-view-user-twitter" class="user-view-user-contact" href="https://www.twitter.com/<?php echo $user->getContactTwitter(); ?>" target="_blank">
-                    <i class="fa fa-twitter tooltip user-view-user-contact-icon" title="Twitter"></i>
-                </a>
+                    <a id="user-view-user-twitter" class="user-view-user-contact" href="https://www.twitter.com/<?php echo $user->getContactTwitter(); ?>" target="_blank">
+                        <i class="fa fa-twitter tooltip user-view-user-contact-icon" title="Twitter"></i>
+                    </a>
                 <?php endif; ?>
                 <?php if ($user->getContactLinkedIn()) : ?>
-                <a id="user-view-user-linkedin" class="user-view-user-contact" href="<?php echo $user->getContactLinkedIn(); ?>" target="_blank">
-                    <i class="fa fa-linkedin tooltip user-view-user-contact-icon" title="Twitter"></i>
-                </a>
+                    <a id="user-view-user-linkedin" class="user-view-user-contact" href="<?php echo $user->getContactLinkedIn(); ?>" target="_blank">
+                        <i class="fa fa-linkedin tooltip user-view-user-contact-icon" title="LinkedIn"></i>
+                    </a>
                 <?php endif; ?>
                 <?php if ($user->getContactCertification()) : ?>
-                <a id="user-view-user-certification" class="user-view-user-contact" href="<?php echo $user->getContactCertification(); ?>" target="_blank">
-                    <i class="fa fa-certificate tooltip user-view-user-contact-icon" title="Magento Certified Developer"></i>
-                </a>
+                    <a id="user-view-user-certification" class="user-view-user-contact" href="<?php echo $user->getContactCertification(); ?>" target="_blank">
+                        <i class="fa fa-certificate tooltip user-view-user-contact-icon" title="Magento Certified Developer"></i>
+                    </a>
                 <?php endif; ?>
                 <?php if ($user->getContactGithub()) : ?>
-                <a id="user-view-user-github" class="user-view-user-contact" href="https://www.github.com/<?php echo $user->getContactGithub(); ?>" target="_blank">
-                    <i class="fa fa-github tooltip user-view-user-contact-icon" title="Magento Certified Developer"></i>
-                </a>
+                    <a id="user-view-user-github" class="user-view-user-contact" href="https://www.github.com/<?php echo $user->getContactGithub(); ?>" target="_blank">
+                        <i class="fa fa-github tooltip user-view-user-contact-icon" title="Github"></i>
+                    </a>
                 <?php endif; ?>
                 <?php if ($user->getContactStackOverflow()) : ?>
-                <a id="user-view-user-stackoverflow" class="user-view-user-contact" href="<?php echo $user->getContactStackOverflow(); ?>" target="_blank">
-                    <i class="fa fa-stack-overflow tooltip user-view-user-contact-icon" title="Magento Certified Developer"></i>
-                </a>
+                    <a id="user-view-user-stackoverflow" class="user-view-user-contact" href="<?php echo $user->getContactStackOverflow(); ?>" target="_blank">
+                        <i class="fa fa-stack-overflow tooltip user-view-user-contact-icon" title="Stack Overflow"></i>
+                    </a>
                 <?php endif; ?>
             </div>
             <span id="user-view-user-about"><?php echo $user->getAbout(); ?></span>
